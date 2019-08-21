d526933b81e0990d44fbdf1be1096eceb73331ac
zgy朱光阳 <gy.zhu@ctrip.com>
2019-08-21 22:45:14 +0800

    均价挂单

--- zgy.js
+++ zgy.js
@@ -5,6 +5,7 @@ period: 1h
 */
 
 var buyOredreIds = [];
+var GylimitAmuount = 50;
 var initaccount = exchange.GetAccount();
 
 function GetTicker() {
@@ -17,7 +18,7 @@ function GetAccountBalance() {
     //Log("账户信息，Balance:", account.Balance, "FrozenBalance:", account.FrozenBalance, "Stocks:",
     //   account.Stocks, "FrozenStocks:", account.FrozenStocks);
     if (account) {
-            return account.Balance;
+        return account.Balance;
     }
     return 0;
 }
@@ -44,45 +45,66 @@ function loginfoAccount() {
     LogStatus('`' + JSON.stringify(table) + '`')
 
 }
-//下单 取消
+var tryCount = 0;
+var avavBlance = 0;
+
+
+function cancleOrder2(orderId) {
+    //    exchange.CancelOrder(buyOredreIds[j]);
+    var result = exchange.CancelOrder(orderId);
+    if (!result) {
+        tryCount += 1;
+        Log("速度手动取消, 取消订单失败@");
+        if (tryCount >= 3) {
+            return true;
+        }
+        cancleOrder2(orderId);
+
+    } else {
+        tryCount = 0;
+        return true;
+    }
+
+
+}
+var buycount = 6;
+
 function orderType(type, price, orderId) {
 
     //买
     if (type == "Buy") {
 
-        var blance = GetAccountBalance();
-        var bool = (blance >= (price * Amount));
+        var blancer = GetAccountBalance();
+        var bool = (blancer >= (price * avavBlance));
+
         if (bool) {
-            var ids = exchange.Buy(price, Amount);
+            var ids = exchange.Buy(price, avavBlance);
             if (ids) {
                 buyOredreIds.push(ids);
                 var newPrice = price - 0.0001;
-                orderType("Buy", newPrice, Amount);
+                buycount += 1;
+                orderType("Buy", newPrice, avavBlance);
             } else {
-                orderType("Buy", price, Amount);
-
+                orderType("Buy", price, avavBlance);
             }
 
-
         } else {
-            var blance2 = GetAccountBalance();
-            var canBuyAmout = blance2 / price;
-            if (canBuyAmout >= limitAmuount) {
+            var blance3 = GetAccountBalance();
+            var canBuyAmout = blance3 / price;
+            if (canBuyAmout >= GylimitAmuount) {
                 var id2 = exchange.Buy(price, parseFloat((canBuyAmout - 1).toFixed(2)));
                 if (id2) {
                     buyOredreIds.push(id2);
                 }
-                //   Sleep(1000);
-                //   for (var i = 0; i < buyOredreIds.length; i++) {
-                //      exchange.CancelOrder(buyOredreIds[i]);
-                //   }
-                //   buyOredreIds.splice(0, buyOredreIds.length);
+
             }
 
 
-            Sleep(1000);
+            Sleep(600);
             for (var j = 0; j < buyOredreIds.length; j++) {
-                exchange.CancelOrder(buyOredreIds[j]);
+                if (cancleOrder2(buyOredreIds[j])) {
+                    continue;
+                }
             }
             buyOredreIds.splice(0, buyOredreIds.length);
             loginfoAccount();
@@ -100,20 +122,22 @@ function orderType(type, price, orderId) {
 
 }
 
-
 function GetDepth(type) {
 
     //获取市场订单价格深度
     var depth = exchange.GetDepth();
     //Asks 卖单价格
-    var sellPrice = depth.Asks[3].Price;
+    var sellPrice = depth.Asks[1].Price;
     //Bids 买单价格
-    var buyPrice = depth.Bids[4].Price;
+    var buyPrice = depth.Bids[3].Price;
     if (type == "Buy") {
+        var blance = GetAccountBalance();
+        avavBlance = parseFloat((blance / (buyPrice * 8)).toFixed(2));
         orderType("Buy", buyPrice);
     } else {
+        Log("速速关注, 卖单已挂@");
         var account = exchange.GetAccount();
-        exchange.Sell(sellPrice, parseFloat(account.Stocks.toFixed(2)));
+        var result = exchange.Sell(sellPrice, parseFloat(account.Stocks.toFixed(2)));
     }
 
 }
@@ -128,7 +152,7 @@ function main() {
     var orders = exchange.GetOrders();
     for (var i = 0; i < orders.length; i++) {
         if (orders[i].Type == 0) {
-            exchange.CancelOrder(orders[i].Id);
+            cancleOrder2(orders[i].Id);
         }
     }
 
@@ -137,7 +161,7 @@ function main() {
         //  GetTicker();
         var account = exchange.GetAccount();
         if (account) {
-            if (account.Stocks < limitAmuount) {
+            if (account.Stocks < GylimitAmuount) {
                 GetDepth("Buy");
 
             } else {
